9f601c9ff6d447c26052ee7d65f1ed44657fb03e
Lindokuhle King <164007390+Lindokuhle99@users.noreply.github.com>
2026-01-11 20:03:23 +0200

    Update bot settings for name and owner details

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Lindo Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Lindo Bot",
+  botOwner: 'Lindokuhle Designs', // Your name
+  ownerNumber: '27657006860', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
